8a9e251f12258fc5b406349526f0e963546e07ef
igorbasko <igor@Igors-MacBook-Pro.local>
2017-09-25 08:16:14 +0300

    Remove unneeded code

--- src/main/DatabaseActor.scala
+++ src/main/DatabaseActor.scala
@@ -61,30 +61,6 @@ class DatabaseActor(actionsFilesPath: String, actionsFilesPrfx: String) extends 
 
     // TODO: Handle the case when the action key is not in the map.
     unfinishedActions.foreach(action => MasterActor.actionsToActors(action.act_type) ! action)
-
-    //    val source: Source[Int, NotUsed] = Source(1 to 100)
-
-//    val factorials = source.scan(BigInt(1))((acc, next) => acc * next)
-//      .zipWith(Source(0 to 100))((num, idx) => s"$idx! = $num")
-//      .throttle(1,1.second, 1, ThrottleMode.shaping)
-//      .runWith(Sink.foreach(println))(materializer)
-//      .onComplete(_ => self ! main.DatabaseActor.ReadyForWork)(context.dispatcher)
-//
-//    def lineSink(fileName: String): Sink[String, Future[IOResult]] =
-//      Flow[String]
-//      .map(s => ByteString(s + "\n"))
-//      .toMat(FileIO.toPath(Paths.get(fileName)))(Keep.right)
-
-//    factorials.map(_.toString).runWith(lineSink("factorial2.txt"))(materializer)
-//      .onComplete(_ => log.info("Written file"))(context.dispatcher)
-
-//    val result: Future[IOResult] =
-//      factorials
-//      .map(num => ByteString(s"$num\n"))
-//      .runWith(FileIO.toPath(Paths.get("factorials.txt")))(materializer)
-//
-//    result.onComplete(res => log.info(s"File is written ${res.get.count} bytes"))(context.system.dispatcher)
-
     log.info("Started !")
   }
 
@@ -108,64 +84,6 @@ class DatabaseActor(actionsFilesPath: String, actionsFilesPrfx: String) extends 
 
   def controlledTermination(): Unit = {
     context.stop(self)
-  }
-
-  /**
-    * This function finds out the latest action file in a specified path and a specified prefix.
-    * The date should be incorporated in the name of the files.
-    * @param actionsPath The path where the action files reside.
-    * @param filePrfx The prefix of the action file. The portion of the name without the date.
-    * @return The full name of the action file (without the path).
-    */
-  def findActionsFileToLoad(actionsPath: String, filePrfx: String) : String = {
-    val listOfFiles = Files.list(Paths.get(actionsPath)).iterator().asScala
-
-    val actionFiles = listOfFiles.filter(_.getFileName.toString.startsWith(filePrfx))
-
-    val latestFile =  if (actionFiles.isEmpty) ""
-    else {
-      val fileNames = actionFiles.map(file => file.getFileName.toString)
-      fileNames.toList.sortBy(x=>x).last
-    }
-
-    log.info(s"Latest file is : $latestFile")
-    latestFile
-  }
-
-  /**
-    * This function opens and loads the actions in the actions file.
-    * Filters out comments.
-    * @param fileName The name of the actions files to open.
-    * @return A list of strings. Each string represents an action.
-    */
-  def loadActionsFile(fileName: String) : Unit = {
-    val file = Paths.get(fileName)
-
-    if (!Files.exists(file)) {
-      log.info(s"Actions file:[$file] doesn't exists creating a new one.")
-      Try(Files.createFile(file)) match {
-        case Success(path) => log.info(s"Actions file created: [$file]")
-        case Failure(ex) =>
-          log.error(s"Error creating file: [$file], reason: ")
-          log.error(ex.getStackTrace.mkString("\n"))
-      }
-    }
-
-    val fileContents =
-      FileIO
-        .fromPath(file)
-        .runWith(Sink.seq[ByteString])(materializer)
-
-    fileContents.onComplete({
-      case Success(lines) =>
-        val actions = lines.map(_.utf8String).mkString.split("\n").filter(!_.startsWith("#")).map(convertToAction)
-        actions.filter(_.isDefined).foreach(self ! _.get)
-      case Failure(excp) =>
-        log.error(s"Error with reading actions file: $fileName")
-        log.error(excp.getStackTrace.mkString("\n"))
-        log.error("Terminating the application....")
-        context.system.terminate
-    })(context.dispatcher)
   }
 
   /**
@@ -204,16 +122,6 @@ class DatabaseActor(actionsFilesPath: String, actionsFilesPrfx: String) extends 
     * @return true if enough parts.
     */
   def validateRawAction(parts: Array[String]) : Boolean = if (parts.length == 5) true else false
-
-  /**
-    * Get the action's key using its first four values: date, time, action, params
-    * @param action The action for which to determine the key.
-    * @return Returns an object of actions key.
-    */
-  def getActionKey(action: DatabaseActor.Action): DatabaseActor.ActionKey = {
-    val stringKey = action.date + action.time + action.act_type + action.params.mkString
-    DatabaseActor.ActionKey(stringKey.hashCode)
-  }
 
   /**
     * Reply to the sender of the query with the actions that are not finished.
