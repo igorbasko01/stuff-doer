8f497498a20adb0e7bcb250f39c76e0fce12386f
igorbasko <igor@Igors-MacBook-Pro.local>
2018-02-17 16:32:22 +0200

    Display all the available projects in the UI.

--- resources/html/AddTask.js
+++ resources/html/AddTask.js
@@ -1,9 +1,19 @@
-// Get all the available projects.
+// Get all the available projects, and display them in the drop down box.
 document.addEventListener('DOMContentLoaded', function () {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+            var projects = this.responseText.split(";");
+            var projectOpts = [];
+            for (var i = 0; i < projects.length; i++) {
+                var prj = projects[i].split(",");
+                var prjId = prj[0];
+                var prjName = prj[1];
+                projectOpts.push("<option value=\""+prjId+"\">"+prjName+"</option>");
+            }
+            console.log(projectOpts);
+
+            $("#project").append(projectOpts.join(""));
         }
     };
 
