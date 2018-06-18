893767548e3e919b93c1eb94037cb4030c077048
igorbasko <igor@Igors-MacBook-Pro.local>
2018-06-18 07:16:42 +0300

    push the docker image to docker hub

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo 'Hello World !'
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push egorebasko/stuff-doer
