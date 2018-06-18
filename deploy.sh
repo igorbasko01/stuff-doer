612c5ae948f7a87cadffd84a5f2857ab61541a7a
igorbasko <igor@Igors-MacBook-Pro.local>
2018-06-18 07:27:31 +0300

    Download and install hyper.sh cli

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@
 echo 'Hello World !'
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push egorebasko/stuff-doer
+
+echo 'Download Hyper.sh'
+wget https://hyper-install.s3.amazonaws.com/hyper-linux-x86_64.tar.gz
+tar xzf hyper-linux-x86_64.tar.gz
