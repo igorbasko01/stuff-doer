bc8572e57b7314697052fe5df94e79a8d449e601
igorbasko <igor@Igors-MacBook-Pro.local>
2018-06-18 08:06:03 +0300

    add some more params for hyper config and login

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ docker push egorebasko/stuff-doer
 echo 'Download Hyper.sh'
 wget https://hyper-install.s3.amazonaws.com/hyper-linux-x86_64.tar.gz
 tar xzf hyper-linux-x86_64.tar.gz
-./hyper config --accesskey "$HYPER_ACCESS" --secretkey "$HYPER_SECRET"
-./hyper login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+./hyper config --accesskey "$HYPER_ACCESS" --secretkey "$HYPER_SECRET" --default-region us-west-1
+./hyper login -e "$DOCKER_HUB_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 ./hyper rm -f stuff-doer
 ./hyper pull egorebasko/stuff-doer
 ./hyper run -d -p 80:9080 -v stuff-igor:/root --name stuff-doer egorebasko/stuff-doer
