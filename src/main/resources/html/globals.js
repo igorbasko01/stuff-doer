c38efc703b5705ae1877bea2534d06fd6c5766ca
igorbasko <igor@Igors-MacBook-Pro.local>
2018-06-28 07:12:30 +0300

    Move function makeRequest to a global file.

--- src/main/resources/html/globals.js
+++ src/main/resources/html/globals.js
@@ -1,1 +1,22 @@
 var baseURL = "/"
+
+function makeRequest(method, url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function() {
+            resolve(xhr);
+        };
+        xhr.onerror = function() {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+function logHttpError(err) {
+    console.error('An error occurred !', err.statusText);
+}
--- src/main/resources/html/script.js
+++ src/main/resources/html/script.js
@@ -282,24 +282,3 @@ function stopTaskRequest() {
     return makeRequest('POST', baseURL + "basched/stopTask?taskid="+currentTask.id)
     .catch(logHttpError);
 }
-
-function makeRequest(method, url) {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.onload = function() {
-            resolve(xhr);
-        };
-        xhr.onerror = function() {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
-            });
-        };
-        xhr.send();
-    });
-}
-
-function logHttpError(err) {
-    console.error('An error occurred !', err.statusText);
-}
