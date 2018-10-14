bde73db59128506a60c8a5224163484bc95165ad
igorbasko01 <igorbasko@gmail.com>
2018-10-14 21:16:57 +0300

    Prepare functions for starting a break

--- src/main/resources/html/BreakTimer.js
+++ src/main/resources/html/BreakTimer.js
@@ -2,14 +2,19 @@ var BreakTimer = {
     beforeBreak: {
         timer: null,
         endTime: null,
-        waitTime: 0.5 * 60 * 1000,
-        startButton: null
+        waitTime: 0.1 * 60 * 1000, // Change it to 5 minutes.
+        startButton: null,
+        origStartFunc: null,
+        globalTimerDisp: null
     },
     enableBreakTimer: function(globalTimerDisp, startButton) {
         var currentTime = new Date().getTime();
         this.beforeBreak.endTime = currentTime + this.beforeBreak.waitTime;
         this.beforeBreak.timer = setInterval(this.breakTimerEnded, 1000);
+        this.beforeBreak.globalTimerDisp = globalTimerDisp;
         this.beforeBreak.startButton = startButton;
+        this.beforeBreak.origStartFunc = startButton.attr("onclick");
+        startButton.attr("onclick", "BreakTimer.startBreak()")
         startButton.text("Start Break");
     },
     breakTimerEnded: function() {
@@ -19,7 +24,20 @@ var BreakTimer = {
         if (currentTime > BreakTimer.beforeBreak.endTime) {
             clearInterval(BreakTimer.beforeBreak.timer);
             console.log("Break wait timer ended...");
-            BreakTimer.beforeBreak.startButton.text("Start");
+            BreakTimer.resetStartButton();
         }
+    },
+    startBreak: function() {
+        console.log("Break started...");
+        clearInterval(BreakTimer.beforeBreak.timer);
+        this.beforeBreak.startButton.text("Skip Break");
+    },
+    stopBreak: function() {
+        
+    },
+    resetStartButton: function() {
+        BreakTimer.beforeBreak.startButton.text("Start");
+        BreakTimer.beforeBreak.startButton.attr("onclick",
+          BreakTimer.beforeBreak.origStartFunc);
     }
 }
